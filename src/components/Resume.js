3f6cb08b1b16945e5b2ebdcbfff82ae7616e0d69
ErikShively <shivelyer@gmail.com>
2023-02-27 23:57:23 -0600

    Fixed resume being set on a fixed size. Also updated resume to a more modern version

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,7 +6,7 @@ function Resume() {
     <div className='resume'>
       {/* <img src="/logo512.png"></img> */}
       {/* <a href="/ResumeErikShively.pdf" download="ResumeErikShively.pdf">download</a> */}
-      <object className="resume_view" data="/ResumeErikShively.pdf" type="application/pdf" width="800" height="500"/>
+      <object className="resume_view" data="/ResumeErikShively.pdf" type="application/pdf" width="65%" height="100%"/>
     </div>
   );
 }
